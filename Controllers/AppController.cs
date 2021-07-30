7ae3856cf59e0b50f563fb3e5f50c6dc58f762bf
Edward E Greenwood <edwardgreenwood38@gmail.com>
2021-07-30 13:52:43 -0400

    manual add of files
    adding files from local folder, except wwwroot

---
+++ Controllers/AppController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using phoneDirectory.Services;
+using phoneDirectory.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace phoneDirectory.Controllers
+{
+    public class AppController : Controller
+    {
+        private readonly IMailService _mailService;
+
+        public AppController(IMailService mailService)
+        {
+            _mailService = mailService;
+        }
+        
+        public IActionResult Index()
+        {
+            //throw new InvalidProgramException("Bad things happen");
+            return View();
+        }
+
+        [HttpGet("contact")]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost("contact")]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // send email
+                _mailService.SendMessage("edward.e.greenwood.ctr@mail.mil", model.Subject, $"From: {model.Name} {model.Email}, Message: {model.Message}");
+                ViewBag.UserMessage = "Mail Sent";
+                ModelState.Clear();
+            }
+            else
+            {
+                // show error
+            }
+            
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewBag.Title = "About Us";
+
+            return View();
+        }
+    }
+}
---
+++ Services/IMailService.cs
@@ -0,0 +1,7 @@
+﻿namespace phoneDirectory.Services
+{
+    public interface IMailService
+    {
+        void SendMessage(string to, string subject, string body);
+    }
+}
