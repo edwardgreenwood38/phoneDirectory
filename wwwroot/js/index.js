3229aceb8dce0b19412098d8e29a606275de3fc9
Edward E Greenwood <edwardgreenwood38@gmail.com>
2021-07-30 14:01:06 -0400

    added files from wwwroot
    added some of the file from wwwroot

---
+++ wwwroot/js/index.js
@@ -0,0 +1,36 @@
+﻿
+
+$(document).ready(function () {
+    console.log("hello pluralsight");
+
+
+
+    var theform = $("#theform");
+    theform.hide();
+
+    var button = $("#buybutton");
+    button.on("click", function () {
+        console.log("Buying Item");
+    });
+
+    var productInfo = $(".product-props li");
+    productInfo.on("click", function () {
+        console.log("You clicked on " + $(this).text());
+    });
+
+    var $loginToggle = $("#loginToggle");
+    var $popupForm = $(".popup-form");
+
+    $loginToggle.on("click", function () {
+        $popupForm.fadeToggle(1000);
+    });
+
+
+
+
+
+});
+    
+
+
+
